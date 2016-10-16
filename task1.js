38e60107646bcd65b1ca6072b9c0f0f9aaa2cc89
metmax <m32max@mail.ru>
2016-10-16 21:27:06 +0300

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,19 @@
+/*Треугольник. Напишите цикл, выводит такой треугольник:
+
+   Источник: http://www.itmathrepetitor.ru/zadachi-po-javascript-iz-knigi-vyrazitelnyjj-javascript/ 
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+var i;
+var x="#";
+console.log(x);
+for (i=0; i<=5; i++) {
+    x=x+"#";
+    console.log(x);
+}
